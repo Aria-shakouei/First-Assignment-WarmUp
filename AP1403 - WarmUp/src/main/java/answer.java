3562a927e16ce97d016b412cc8752e30d9980514
Aria shakouei <arya.shakooee@gmail.com>
2025-03-03 19:01:47 +0330

    add file: answer.java

---
+++ AP1403 - WarmUp/src/main/java/answer.java
@@ -0,0 +1,87 @@
+import java.util.Scanner;
+
+public class answer {
+
+    /*
+        complete this function to check if the input number is prime or not
+     */
+    public boolean isPrime(long n) {
+        // todo
+        if (n <= 1) return true;
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) return true;
+        }
+        return false;
+    }
+
+    /*
+        implement an algorithm to find out the index of input number in a fibonacci sequence starting from 0, 1
+        e.g. 0, 1, 1, 2, 3, 5, ...
+        indices start from 0, e.g. 3 is the index 4 of this sequence
+        if the input is not a fibonacci number with description above, return -1
+     */
+    public long fibonacciIndex(long n) {
+        // todo
+        if (n < 0) return -1;
+        long a = 0, b = 1, index = 0;
+
+        while (a <= n) {
+            if (a == n) return index;
+
+            long c = a + b;
+            a = b;
+            b = c;
+            index++;
+        }
+        return -1;
+    }
+
+    /*
+        you should create a triangle with "*" and return a two-dimensional array of characters based on that
+        the triangle's area is empty, which means some characters should be " "
+
+        example 1, input = 3:
+        *
+        **
+        ***
+
+        example 2, input = 5:
+        *
+        **
+        * *
+        *  *
+        *****
+
+        the output has to be a two-dimensional array of characters, so don't just print the triangle!
+     */
+    public char[][] generateTriangle(int n) {
+        // todo
+        char[][] triangle = new char[n][n];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n ; j++) {
+                if (j <= i) {
+                    if (i == n - 1 || j == 0 || j == i) {
+                        triangle[i][j] = '*';
+                    } else {
+                        triangle[i][j] = ' ';
+                    }
+                } else {
+                    triangle[i][j] = ' ';
+                }
+            }
+        }
+
+        for (int i = 0; i < triangle.length; i++) {
+            for (int j = 0; j < triangle[i].length; j++) {
+                System.out.print(triangle[i][j]);
+            }
+            System.out.println();
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        // you can test your code here, but then it should be checked with test cases4
+    }
+}
